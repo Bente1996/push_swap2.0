ae997f2a381259cf89c0da48da585fee8a40b295
bede-kon <bede-kon@studeent.codam.nl>
2026-03-28 23:23:53 +0100

    big_list onderste en bovenste helft werken apart

--- big_list.c
+++ big_list.c
@@ -7,35 +7,28 @@ void	big_list(t_node **stack_a, t_node **stack_b, int half) // werkt met + 1
 	int	quarter;
 	int	three_quarter;
 	int	tq;
-//	int	h;
-//	int	swap;
+	int	h;
+	int	swap;
 	int	swap_rot;
 
 	whole = half * 2;
 	quarter = half / 2;
 	three_quarter = whole - quarter;
 	tq = three_quarter + 1;
-//	h = half + 1;
-//	swap = 0;
+	h = half + 1;
+	swap = 0;
 	swap_rot = 0;
-	printf("Tussenstand:\n");
-	printf("Halve:%d\n", half); // 49
-	printf("Kwartje:%d\n", quarter); //24
-	printf("TQ:%d\n", three_quarter); //74
+//	printf("Tussenstand:\n");
+//	printf("Halve:%d\n", half); // 49
+//	printf("Kwartje:%d\n", quarter); //24
+//	printf("TQ:%d\n", three_quarter); //74
 //	print_list(*stack_a, 'A');
 //	print_list(*stack_b, 'B');	
 	t_node	*bottom_stack = NULL;
-//	t_node	*top_stack = NULL;
-	int	n = 20050;
+	int	n = 80000;
 	while (*stack_a && n) // alles naar b: 73_50 0-49(random) 74_99
 	{
 		n--;
-		printf("\nstack_a: %d\n", (*stack_a)->sorted_index);
-		printf("stack_b: %d\n", (*stack_b)->sorted_index);
-		printf("tq: %d\n", tq);
-		printf("swap_rot: %d\n", swap_rot);
-		if (bottom_stack)
-			printf("bottom_stack: %d\n", bottom_stack->sorted_index);
 		if (((*stack_a)->sorted_index  == tq || ((*stack_a)->sorted_index == tq + 1) || ((*stack_a)->sorted_index == tq + 2)) && (*stack_a)->sorted_index > three_quarter) // 75->99
 		{
 
@@ -85,37 +78,73 @@ void	big_list(t_node **stack_a, t_node **stack_b, int half) // werkt met + 1
 				tq++;
 			}
 		}
-	//	else if (((*stack_a)->sorted_index == h || ((*stack_a)->sorted_index == h + 1 ) || ((*stack_a)->sorted_index == h + 2)) && (*stack_a)->sorted_index <= three_quarter) // 50->74	
-	//	{
-	//		if ((((*stack_a)->sorted_index == h + 1) || ((*stack_a)->sorted_index == h + 2)) && swap < 2)
-	//		{
-	//			pb(stack_a, stack_b);
-	//			if (!swap || (swap == 1 && (*stack_b)->sorted_index > top_stack->sorted_index))
-	//				top_stack = *stack_b;
-	//			swap++;
-	//		}
-	//		else if (swap) // als er iets staat wat er niet hoort
-	//		{
-	//			if (((*stack_b)->sorted_index != (*stack_a)->sorted_index - 1) || swap == 2) // als degene die er staat niet opvolgt of als er 2 staan
-	//				rb(stack_b);
-	//			if (swap == 2) // 1 of 2 naar benee
-	//				rb(stack_b);
-	//			pb(stack_a, stack_b); // h op zn plek gezet
-	//			while (swap)
-	//			{
-	//				rrb(stack_b);
-	//				swap--; //gaat naar 0
-	//				h++;
-	//			}
-	//			if (*stack_b != top_stack) // swap indien nodig, wanneer grootste boven zit
-	//				sb(stack_b);
-	//		}
-	//		else // stond niks
-	//		{
-	//			pb(stack_a, stack_b); // gewoon even duwen zonder nadenken weetjewel
-	//			h++;
-	//		}
-	//	}
+		if (((*stack_a)->sorted_index == h || ((*stack_a)->sorted_index == h + 1 ) || ((*stack_a)->sorted_index == h + 2)) && (*stack_a)->sorted_index <= three_quarter) // 50->74	
+		{
+			printf("stack_a: %d\n", (*stack_a)->sorted_index);
+			printf("stack_b: %d\n", (*stack_b)->sorted_index);
+			printf("h: %d\n", h);
+			printf("swap:%d\n", swap);
+			if ((((*stack_a)->sorted_index == h + 1) || ((*stack_a)->sorted_index == h + 2)) && swap < 2)
+			{
+				pb(stack_a, stack_b);
+				swap++;
+			}
+			else if (swap && (*stack_a)->sorted_index == h) // als er iets staat wat er niet hoort
+			{
+				if (swap == 1)
+				{
+					pb(stack_a, stack_b); // h op zn plek gezet
+					sb(stack_b);
+					if ((*stack_b)->sorted_index == (*stack_b)->next->sorted_index + 1)
+					{
+						swap--;
+						h++;
+					}
+				}
+				else  // als swap == 2
+				{
+					rb(stack_b);
+					if ((*stack_b)->sorted_index == (*stack_a)->sorted_index + 1)
+					{
+						pb(stack_a, stack_b);
+						sb(stack_b);
+						swap--;
+						h++;
+						rrb(stack_b);
+						if ((*stack_b)->sorted_index == (*stack_b)->next->sorted_index + 1)
+						{
+							swap--;
+							h++;
+						}
+					}
+					else
+					{
+						rb(stack_b);
+						pb(stack_a, stack_b);
+						rrb(stack_b);
+						rrb(stack_b);
+						if ((*stack_b)->sorted_index == (*stack_b)->next->next->sorted_index + 1)
+						{
+							sb(stack_b);
+							swap--;
+							h++;
+							if ((*stack_b)->sorted_index == (*stack_b)->next->sorted_index + 1)
+							{
+								swap--;
+								h++;
+							}
+						}
+					}
+
+				}
+				h++;
+			}
+			else if ((*stack_a)->sorted_index == h)// stond niks
+			{
+				pb(stack_a, stack_b); // gewoon even duwen zonder nadenken weetjewel
+				h++;
+			}
+		}
 		else // was niet de goeie
 			ra(stack_a);
 	}
